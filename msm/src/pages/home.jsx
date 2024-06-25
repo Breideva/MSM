7df727b6172fb0dd91182042fba048fbae0b7767
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-06-25 17:07:29 -0500

    Finished initial header home page

--- msm/src/pages/home.jsx
+++ msm/src/pages/home.jsx
@@ -1,12 +1,35 @@
-import { Navbar } from "../components/navbar"
-import "../styles/navbar.css"
+import { Navbar } from "../components/navbar";
+import "../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
     <>
-    < Navbar/>
+      <Navbar />
+      <div className="home-header">
+        <h1>Unleash your boat's potential</h1>
+        <h3>Explore our reliable services</h3>
+      </div>
+      <div className="header-boxes">
+      <Link className="box-link">
+        <div className="header-box">
+          <h2>Check out services</h2>
+          <p>View work and services we provide</p>
+        </div>
+        </Link>
+        <Link className="box-link"> 
+        <div className="header-box">
+          <h2>View Projects</h2>
+          <p>Look at amazing work previously done by us</p>
+        </div>
+        </Link>
+        <Link className="box-link">
+        <div className="header-box">
+          <h2>Learn about us</h2>
+          <p>Get to know the ones who help you</p>
+        </div>
+        </Link>
+      </div>
     </>
-  )
-}
-
-
+  );
+};

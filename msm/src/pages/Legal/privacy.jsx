4da276772f64dffdb8e28352f1d67e465250091a
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-07-30 16:27:46 -0500

    Added legality pages and styles for them

---
+++ msm/src/pages/Legal/privacy.jsx
@@ -0,0 +1,304 @@
+import "../../styles/Legal/legal.css";
+
+import { Layout } from "../../components/layout";
+
+export const Privacy = () => {
+  return (
+    <Layout>
+      <div className="legal-content">
+        <div className="legal-text">
+          <h2>
+            <b>Privacy Policy</b>
+          </h2>
+          <p>
+            <b>Effective date:</b> July 23, 2024
+          </p>
+          <p>
+            <b>1. Introduction</b>
+          </p>
+          <p>
+            Welcome to <b>Marine Services and Management</b>.
+          </p>
+          <p>
+            <b>Marine Services and Management</b> (“we,” “us,” or “our”)
+            operates <b>msmboatcare.com</b> (hereinafter referred to as the{" "}
+            <b>“Service”</b>
+            ).
+          </p>
+          <p>
+            This Privacy Policy outlines how we collect, use, and protect your
+            information when you use our Service. By using our Service, you
+            consent to the collection and use of information in accordance with
+            this Privacy Policy. Terms used in this Privacy Policy have the same
+            meanings as in our Terms and Conditions unless otherwise defined
+            here.
+          </p>
+          <p>
+            Our Terms and Conditions (<b>“Terms”</b>) govern all use of our
+            Service and, together with this Privacy Policy, constitute your
+            agreement with us (<b>“Agreement”</b>).
+          </p>
+          <p>
+            <b>2. Definitions</b>
+          </p>
+          <p>
+            <b>SERVICE</b> refers to the msmboatcare.com website operated by
+            Marine Services and Management.
+          </p>
+          <p>
+            <b>PERSONAL DATA</b> means any information that relates to an
+            identified or identifiable individual.
+          </p>
+          <p>
+            <b>USAGE DATA</b> is data collected automatically, either generated
+            by the use of the Service or from the Service infrastructure itself
+            (e.g., the duration of a page visit).
+          </p>
+          <p>
+            <b>COOKIES</b> are small files stored on your device (computer or
+            mobile device).
+          </p>
+          <p>
+            <b>DATA CONTROLLER</b> is the person or entity that determines the
+            purposes and means of processing personal data. For the purposes of
+            this Privacy Policy, we are the Data Controller of your data.
+          </p>
+          <p>
+            <b>DATA PROCESSORS (OR SERVICE PROVIDERS)</b> are individuals or
+            entities who process data on behalf of the Data Controller. We may
+            use third-party Service Providers to process your data more
+            effectively.
+          </p>
+          <p>
+            <b>DATA SUBJECT</b> is an individual whose Personal Data is being
+            processed.
+          </p>
+          <p>
+            <b>THE USER</b> is the individual using our Service, corresponding
+            to the Data Subject.
+          </p>
+          <p>
+            <b>3. Information Collection and Use</b>
+          </p>
+          <p>
+            We collect various types of information for different purposes to
+            provide and improve our Service. If you send us information via
+            email or by completing a "contact" form, we may collect and store
+            your email address along with any other details you provide. This
+            information might be used to reach out to you in the future via
+            mail, email, or phone to share information about our products,
+            services, or solutions that could be of interest to you. Forms
+            submitted through{" "}
+            <a href="https://web3forms.com/privacy">web3forms</a>.
+          </p>
+          <p>
+            <b>4. Types of Data Collected</b>
+          </p>
+          <p>
+            <b>Personal Data</b>
+          </p>
+          <p>
+            While using our Service, we may ask you to provide us with certain
+            personally identifiable information that can be used to contact or
+            identify you (“Personal Data”). This may include, but is not limited
+            to:
+          </p>
+          <ul>
+            <li>Email address</li>
+            <li>First name and last name</li>
+            <li>Phone number</li>
+            <li>Address, Country, State, Province, ZIP/Postal code, City</li>
+            <li>Cookies and Usage Data</li>
+          </ul>
+          <p>
+            We may use your Personal Data to contact you with newsletters,
+            marketing, or promotional materials. You can opt out by contacting
+            us at <b>MarineServicesAndManagement@msmboatcare.com</b>
+          </p>
+          <p>
+            <b>Usage Data</b>
+          </p>
+          <p>
+            We may collect information that your browser sends whenever you
+            visit our Service or access it via any device (“Usage Data”). This
+            may include details such as your computer’s IP address, browser
+            type, pages visited, time and date of visit, and other diagnostic
+            data.
+          </p>
+          <p><b>Examples of Cookies we use include:</b></p>
+              <p>
+                We do not use cookies on our website. However, we may still
+                collect certain types of data to improve our service and user
+                experience.
+              </p>
+          <p>
+            <b>Other Data</b>
+          </p>
+          <p>
+            We may also collect additional information such as sex, age, date of
+            birth, place of birth, passport details, citizenship, registration
+            at place of residence, telephone numbers, education and employment
+            details, and other data.
+          </p>
+          <p>
+            <b>5. Use of Data</b>
+          </p>
+          <p>We use the collected data for various purposes, including:</p>
+          <ul>
+            <li>To provide and maintain our Service</li>
+            <li>To notify you of changes to our Service</li>
+            <li>To allow interactive features</li>
+            <li>To provide customer support</li>
+            <li>To analyze and improve our Service</li>
+            <li>To monitor Service usage</li>
+            <li>To address technical issues</li>
+            <li>To fulfill other purposes you provide it for</li>
+            <li>To enforce our contracts and policies</li>
+            <li>To send updates, special offers, and other information</li>
+          </ul>
+          <p>
+            <b>6. Retention of Data</b>
+          </p>
+          <p>
+            We retain your Personal Data only for as long as necessary for the
+            purposes set out in this Privacy Policy. We may retain Usage Data
+            for internal analysis and to comply with legal obligations.
+          </p>
+          <p>
+            <b>7. Transfer of Data</b>
+          </p>
+          <p>
+            Your information, including Personal Data, may be transferred to and
+            maintained on computers located outside your jurisdiction. By
+            providing information to us, you consent to the transfer of your
+            data to the United States, where it will be processed.
+          </p>
+          <p>
+            <b>8. Disclosure of Data</b>
+          </p>
+          <p>
+            We may disclose your Personal Data in the following circumstances:
+          </p>
+          <ul>
+            <li>
+              <b>For Law Enforcement:</b> If required by law or public
+              authorities.
+            </li>
+            <li>
+              <b>Business Transactions:</b> In the event of a merger,
+              acquisition, or asset sale.
+            </li>
+            <li>
+              <b>Other Cases:</b> To subsidiaries, contractors, service
+              providers, or as otherwise necessary.
+            </li>
+          </ul>
+          <p>
+            <b>9. Security of Data</b>
+          </p>
+          <p>
+            We strive to protect your data using commercially acceptable means,
+            but cannot guarantee absolute security.
+          </p>
+          <p>
+            <b>10. Your Data Protection Rights Under GDPR</b>
+          </p>
+          <p>
+            If you are in the EU or EEA, you have rights under the GDPR
+            including access, rectification, deletion, restriction, data
+            portability, and withdrawal of consent. To exercise these rights,
+            contact us at <b>MarineServicesAndManagement@msmboatcare.com</b>.
+          </p>
+          <p>
+            <b>11. Your Data Protection Rights Under CalOPPA</b>
+          </p>
+          <p>Under CalOPPA, we agree to:</p>
+          <ul>
+            <li>Allow users to visit our site anonymously.</li>
+            <li>
+              Include “Privacy” in the Privacy Policy link on our home page.
+            </li>
+            <li>
+              Notify users of any policy changes on our Privacy Policy page.
+            </li>
+            <li>
+              Allow users to change their personal information by contacting us.
+            </li>
+          </ul>
+          <p>
+            We honor Do Not Track signals. You can enable or disable Do Not
+            Track via your browser settings.
+          </p>
+          <p>
+            <b>12. Your Data Protection Rights Under CCPA</b>
+          </p>
+          <p>
+            If you are a California resident, you have rights to know what
+            personal information we have, request deletion, and stop sales of
+            your data. To exercise these rights, contact us at{" "}
+            <b>MarineServicesAndManagement@msmboatcare.com</b>.
+          </p>
+          <p>
+            <b>13. Service Providers</b>
+          </p>
+          <p>
+            We may use third-party Service Providers to facilitate our Service
+            and process data on our behalf.
+          </p>
+          <p>
+            <b>14. Analytics</b>
+          </p>
+          <p>
+            We may use third-party Service Providers to monitor and analyze
+            Service usage.
+          </p>
+          <p>
+            <b>15. CI/CD Tools</b>
+          </p>
+          <p>
+            We may use third-party Service Providers for automated development
+            processes.
+          </p>
+          <p>
+            <b>16. Behavioral Remarketing</b>
+          </p>
+          <p>
+            We may use remarketing services to advertise to you on third-party
+            websites.
+          </p>
+          <p>
+            <b>17. Links to Other Sites</b>
+          </p>
+          <p>
+            Our Service may contain links to other sites. We are not responsible
+            for the privacy practices of those sites. Please review their
+            privacy policies.
+          </p>
+          <p>
+            <b>18. Children’s Privacy</b>
+          </p>
+          <p>
+            Our Service is not intended for individuals under 18. We do not
+            knowingly collect data from children. If we become aware of such
+            data, we will take steps to delete it.
+          </p>
+          <p>
+            <b>19. Changes to This Privacy Policy</b>
+          </p>
+          <p>
+            We may update this Privacy Policy from time to time. We will notify
+            you of changes by posting the new policy on this page. Please review
+            it periodically.
+          </p>
+          <p>
+            <b>20. Contact Us</b>
+          </p>
+          <p>
+            If you have any questions about this Privacy Policy, please contact
+            us at <b>MarineServicesAndManagement@msmboatcare.com</b>.
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
---
+++ msm/src/pages/Legal/terms.jsx
@@ -0,0 +1,250 @@
+import "../../styles/Legal/legal.css";
+
+import { Layout } from "../../components/layout";
+
+
+export const Terms = () => {
+  return (
+    <Layout>
+    <div className="legal-content">
+      <div className="legal-text">
+        <h2>
+          <b>TERMS AND CONDITIONS</b>
+        </h2>
+        <p>Last updated: 2024-07-23</p>
+        <p>
+          1. <b>Introduction</b>
+        </p>
+        <p>
+          Welcome to <b>Marine Services and Management</b> (“Company”, “we”,
+          “our”, “us”)!
+        </p>
+        <p>
+          These Terms of Service (“Terms”, “Terms of Service”) govern your use
+          of our website located at <b>msmboatcare.com</b> (together or
+          individually “Service”) operated by{" "}
+          <b>Marine Services and Management</b>.
+        </p>
+        <p>
+          Our Privacy Policy also governs your use of our Service and explains
+          how we collect, safeguard, and disclose information resulting from
+          your use of our web pages.
+        </p>
+        <p>
+          By using our Service, you agree to be bound by these Terms and our
+          Privacy Policy (“Agreements”). If you do not agree with (or cannot
+          comply with) the Agreements, you may not use the Service. Please
+          contact us at <b>MarineServicesAndManagement@msmboatcare.com</b> if
+          you have any questions or concerns.
+        </p>
+        <p>
+          2. <b>Communications</b>
+        </p>
+        <p>
+          By using our Service, you consent to receiving newsletters, marketing
+          or promotional materials, and other information from us. You can opt
+          out of these communications at any time by emailing <b>MarineServicesAndManagement@msmboatcare.com</b>
+          .
+        </p>
+        <p>
+          3. <b>Contests, Sweepstakes, and Promotions</b>
+        </p>
+        <p>
+          Promotions through our Service may have separate rules. Please review
+          the applicable rules and our Privacy Policy. In case of conflict
+          between Promotion rules and these Terms, Promotion rules will take
+          precedence.
+        </p>
+        <p>
+          4. <b>Refunds</b>
+        </p>
+        <p>
+          We offer refunds for Contracts within <b>30 days</b> of the original
+          purchase date. Contact us by emailing <b>MarineServicesAndManagement@msmboatcare.com</b>
+        </p>
+        <p>
+          5. <b>Content</b>
+        </p>
+        <p>
+          Content on our Service is the property of Marine Services and
+          Management or used with permission. You may not use this Content for
+          commercial purposes or personal gain without our prior written
+          consent.
+        </p>
+        <p>
+          6. <b>Prohibited Uses</b>
+        </p>
+        <p>
+          You agree not to use our Service in any unlawful manner or for
+          prohibited purposes. This includes, but is not limited to, violating
+          laws, harming minors, sending spam, impersonating others, or
+          introducing harmful materials.
+        </p>
+        <p>
+          You also agree not to interfere with the proper functioning of our
+          Service, including using automated tools to access or monitor the
+          Service or introducing harmful components like viruses.
+        </p>
+        <p>
+          7. <b>Analytics</b>
+        </p>
+        <p>
+          We may use third-party Service Providers to monitor and analyze
+          Service usage.
+        </p>
+        <p>
+          8. <b>No Use By Minors</b>
+        </p>
+        <p>
+          Our Service is intended for users who are at least eighteen (18) years
+          old. By using our Service, you confirm that you are at least eighteen
+          (18) years old.
+        </p>
+        <p>
+          9. <b>Intellectual Property</b>
+        </p>
+        <p>
+          Our Service and its original content are owned by Marine Services and
+          Management and protected by intellectual property laws. You may not
+          use our trademarks without prior written consent.
+        </p>
+        <p>
+          10. <b>Copyright Policy</b>
+        </p>
+        <p>
+          We respect intellectual property rights. If you believe that your
+          copyrighted work is being infringed, please send a detailed
+          description of the alleged infringement to{" "}
+          <b>MarineServicesAndManagement@msmboatcare.com</b> with the subject
+          line “Copyright Infringement.”
+        </p>
+        <p>
+          You may be liable for damages if you make a misrepresentation about
+          copyright infringement.
+        </p>
+        <p>
+          11. <b>DMCA Notice and Procedure for Copyright Infringement Claims</b>
+        </p>
+        <p>
+          To file a DMCA notice, provide our Copyright Agent with the following
+          information:
+        </p>
+        <ul>
+          <li>
+            An electronic or physical signature of the person authorized to act
+            on behalf of the copyright owner;
+          </li>
+          <li>
+            A description of the copyrighted work that you claim has been
+            infringed;
+          </li>
+          <li>
+            Identification of the URL or specific location of the material you
+            claim is infringing;
+          </li>
+          <li>Your contact information;</li>
+          <li>
+            A statement that you believe the use of the material is not
+            authorized;
+          </li>
+          <li>
+            A statement that the information provided is accurate and that you
+            are the copyright owner or authorized to act on their behalf.
+          </li>
+        </ul>
+        <p>
+          Contact our Copyright Agent via email at{" "}
+          <b>MarineServicesAndManagement@msmboatcare.com</b>.
+        </p>
+        <p>
+          12. <b>Error Reporting and Feedback</b>
+        </p>
+        <p>
+          You may provide feedback concerning our Service by emailing{" "}
+          <b>MarineServicesAndManagement@msmboatcare.com</b>. You acknowledge
+          that we may use your feedback without any obligation to you, and you
+          grant us the right to use it as described in our Privacy Policy.
+        </p>
+        <p>
+          13. <b>Links To Other Websites</b>
+        </p>
+        <p>
+          Our Service may contain links to third-party websites. We do not
+          control these websites and are not responsible for their content,
+          privacy policies, or practices. You access these sites at your own
+          risk.
+        </p>
+        <p>
+          14. <b>Disclaimer Of Warranty</b>
+        </p>
+        <p>
+          Our Service is provided “as is” and “as available.” We do not make any
+          warranties regarding the Service's operation or its content. Your use
+          of the Service is at your own risk.
+        </p>
+        <p>
+          15. <b>Limitation Of Liability</b>
+        </p>
+        <p>
+          To the fullest extent permitted by law, we are not liable for any
+          indirect, punitive, special, incidental, or consequential damages
+          arising from your use of our Service. Our liability will be limited to
+          the amount you paid for the products or services.
+        </p>
+        <p>
+          16. <b>Termination</b>
+        </p>
+        <p>
+          We may terminate or suspend your access to our Service at our
+          discretion, without notice, for any reason. Upon termination, all
+          provisions that should survive termination will remain in effect.
+        </p>
+        <p>
+          17. <b>Governing Law</b>
+        </p>
+        <p>
+          These Terms are governed by the laws of the United States. Any
+          disputes will be handled in accordance with these laws.
+        </p>
+        <p>
+          18. <b>Changes To Service</b>
+        </p>
+        <p>
+          We may withdraw or amend our Service at our discretion without notice.
+          We are not liable if any part of the Service is unavailable.
+        </p>
+        <p>
+          19. <b>Amendments To Terms</b>
+        </p>
+        <p>
+          We may update these Terms by posting the changes on this site. Your
+          continued use of the Service after changes indicates your acceptance
+          of the new Terms.
+        </p>
+        <p>
+          20. <b>Waiver And Severability</b>
+        </p>
+        <p>
+          No waiver of any term by us constitutes a continuing waiver. If any
+          provision is held invalid, the remaining provisions will continue in
+          effect.
+        </p>
+        <p>
+          21. <b>Acknowledgement</b>
+        </p>
+        <p>
+          By using our Service, you acknowledge that you have read and agree to
+          these Terms of Service.
+        </p>
+        <p>
+          22. <b>Contact Us</b>
+        </p>
+        <p>
+          For any feedback, comments, or support requests, please email us at{" "}
+          <b>MarineServicesAndManagement@msmboatcare.com</b>.
+        </p>
+      </div>
+    </div>
+    </Layout>
+  );
+};
